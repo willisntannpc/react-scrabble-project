0be0ec59d25cdf03858548edd9062ac2b6634023
Anita Wangui <anitawangui22@gmail.com>
2024-11-13 15:38:31 +0300

    add instructions and vertical play

---
+++ src/Components/Vertical.js
@@ -0,0 +1,12 @@
+import TileRack from "./TileRack";
+import Board from "./Board";
+function Vertical() {
+  return (
+    <div>
+      <h1>Vertical Play</h1>
+      <Board />
+      <TileRack/>
+    </div>
+  );
+}
+export default Vertical;
