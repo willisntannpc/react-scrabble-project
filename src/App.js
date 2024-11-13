fec163d279fd59f830590786608c2d3c2d49cad7
Anita Wangui <anitawangui22@gmail.com>
2024-11-13 14:56:59 +0300

    add tile rack and tiles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Board from './Components/Board';
-
+import Horizontal from './Components/Horizontal';
 function App() {
     return (
         <div>
             <h1>Scrabble Game</h1>
             <Board />
+            <Horizontal/>
         </div>
     );
 }
